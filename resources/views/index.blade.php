719c9e788d6e7bd6b228537aeeef7bb402e3eb9a
alikariminejad <kariminejad.ali.ak@gmail.com>
2024-12-07 23:54:30 +0330

    Routes

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+<a href="{{ route ('home') }}">Home</a>
+<a href="{{ route ('basics.about-us') }}">About Us</a>
+<a href="{{ route ('basics.contact-us') }}">Contact Us</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+})->name('home');
+
+Route::get('/login', function(){
+    return 'Login Page';
+})->name('login');
+
+Route::prefix('user')->name('basics.')->middleware(['web', 'web', 'throttle:4,1'])->group(function(){
+    Route::get('/about-us', function(){
+        return 'About Us';
+    })->name('about-us');
+    Route::get('/contact-us', function(){
+        return 'Contact Us';
+    })->name('contact-us');
 });
 
 Route::middleware([
