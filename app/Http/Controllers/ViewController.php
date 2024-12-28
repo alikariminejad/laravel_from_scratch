0ccb701883ee5f378d59acdd7a10b36f71ee7566
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2024-12-28 13:52:08 +0330

    working with blade

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -7,9 +7,14 @@ use Illuminate\Http\Request;
 class ViewController extends Controller
 {
     public function index(){
-        $data['title']='Learning About View';
-        $data['description'] = '<h1>why do we use it?</h1>Lorem Ipsum is simply dummy text of the printing';
-        $data['comment'] = "<script>alert('Hello You are hacked')</script>";
-        return view('views.index', $data);
+        $title = 'Bootstrap Conversion';
+        return view('views.index', compact('title'));
     }
+    // public function index(){
+    //     $data['title']='Learning About View';
+    //     $data['description'] = '<h1>why do we use it?</h1>Lorem Ipsum is simply dummy text of the printing';
+    //     $data['comment'] = "<script>alert('Hello You are hacked')</script>";
+    //     return view('views.index', $data);
+    // }
+
 }
