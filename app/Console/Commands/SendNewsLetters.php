9b9a94421bb4a0775e9cd5589dff1217b1119c9c
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2024-12-29 12:55:45 +0330

    user input and prompts

--- app/Console/Commands/SendNewsLetters.php
+++ app/Console/Commands/SendNewsLetters.php
@@ -4,18 +4,22 @@ namespace App\Console\Commands;
 
 use App\Models\Post;
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Illuminate\Support\Facades\Log;
 
-class SendNewsLetters extends Command
+class SendNewsLetters extends Command implements PromptsForMissingInput
 {
 
-    protected $signature = 'send:newletters';
+    protected $signature = 'send:newletters {user}';
 
     protected $description = 'This is used to send news letters to all the subscribed users.';
 
     public function handle()
     {
-        $name = $this -> ask('What is your name?');
-        $this -> info('My name is: '. $name);
+        // $name = $this -> ask('What is your name?');
+        // $this -> info('My name is: '. $name);
+
+        $user_id = $this -> arguement('user');
+        $this -> info('User ID: ' . $user_id);
     }
 }
