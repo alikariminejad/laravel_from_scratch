a1ce036416540e5360c3c9bd7ad5ba1f9d1a982c
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-01-01 10:24:50 +0330

    Collections

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -18,7 +18,7 @@ class CollectionController extends Controller{
         //     return Str::upper($name);
         // }) -> reject(function($name){
         //     return empty($name);
-        // }); 
+        // });
         // dd($collection -> all());
     //     $collection =  collect([100,20,39,405,555,62,7,88,99,10]) -> sort();
     //     $collection = $collection -> chunk(3);
@@ -48,7 +48,41 @@ class CollectionController extends Controller{
         // $collection1 = collect([3,4,5,6,7]);
         // dd($collection -> diff($collection1));
 
-        $collection = collect(['a', 'b', 'a', 'c', 'b']);
-        dd($collection -> duplicates());
-    
+//        $collection = collect(['a', 'b', 'a', 'c', 'b']);
+//        dd($collection -> duplicates());
+//        $collection = collect([100,20,39,405,555,62,7,88,99,10])->filter(function($numbers){
+//            return $numbers < 100;
+//        });
+//        dd($collection -> sort() -> all());
+
+//        $collection = collect([
+//            'name' => 'Ali',
+//            'Language' => [
+//                'php', 'javascript'
+//            ]
+//        ]) -> flatten();
+//        dd($collection -> all());
+//        $collection = collect([
+//                                ['account_id' => 'account-x10', 'product' => 'Chair'],
+//                                ['account_id' => 'account-x10', 'product' => 'Bookcase'],
+//                                ['account_id' => 'account-x11', 'product' => 'Desk']
+//        ]) -> groupBy('account_id');
+//        dd($collection -> all());
+//    $collection = collect(['account_id' => 1, 'product' => 'Desk', 'amount' => 5]) -> has('product');
+//    dd($collection);
+//        $collection = collect([
+//                                ['product_id' => 'prod-100', 'name' => 'Desk'],
+//                                ['product_id' => 'prod-200', 'name' => 'Laptop'],
+//        ]);
+//        dd($collection-> pluck('product_id') -> all());
+        $collection = collect([
+                                ['product' => 'Desk', 'price' => 100],
+                                ['product' => 'Desk_4', 'price' => 200],
+                                ['product' => 'Desk_3', 'price' => 300],
+                                ['product' => 'Desk_2', 'price' => 400],
+        ]);
+//        dd($collection-> where('price', '<', 300) -> all());
+        dd($collection-> whereBetween('price', [200, 300]) -> all());
+
     }}
+
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/', [\App\Http\Controllers\ViewController::class, 'index'])->name('home')->middleware('auth');
+Route::get('/', [\App\Http\Controllers\ViewController::class, 'index'])->name('home');
+//Route::get('/', [\App\Http\Controllers\ViewController::class, 'index'])->name('home')->middleware('auth');
 Route::get('/about-us', [\App\Http\Controllers\ViewController::class,'about_us'])->name('about-us');
 Route::get('/contact', [\App\Http\Controllers\ViewController::class,'contact'])->name('contact');
 
